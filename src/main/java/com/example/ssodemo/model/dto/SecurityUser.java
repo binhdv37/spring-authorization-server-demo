1015083c823091a3d85ee2433cb7e880b3548436
binhdv37 <vanbinhdoan971@gmail.com>
2023-10-29 23:58:52 +0700

    fix: fix parse user issue
    Signed-off-by: binhdv37 <vanbinhdoan971@gmail.com>

---
+++ src/main/java/com/example/ssodemo/model/dto/SecurityUser.java
@@ -0,0 +1,114 @@
+package com.example.ssodemo.model.dto;
+
+import com.example.ssodemo.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SecurityUser implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+
+    public SecurityUser() {
+    }
+
+    public SecurityUser(Long id, String username, String password, String email, String fullName, String phoneNumber) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public SecurityUser(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.fullName = user.getFullName();
+        this.phoneNumber = user.getPhoneNumber();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/main/java/com/example/ssodemo/service/UserDetailServiceImpl.java
+++ src/main/java/com/example/ssodemo/service/UserDetailServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.ssodemo.service;
 
 import com.example.ssodemo.model.User;
+import com.example.ssodemo.model.dto.SecurityUser;
 import com.example.ssodemo.repo.UserRepository;
 import com.sunteco.suncloud.lib.exception.StcException;
-import com.sunteco.suncloud.lib.model.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,19 +23,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
         if (user.getPassword() == null) {
             throw new StcException("incorect username or password", "incorect username or password");
         }
-        UserInfo result = new UserInfo(user.getUsername(), user.getPassword());
-        result.setFullName(user.getFullName());
-        result.setId(user.getId());
-        result.setEmail(user.getEmail());
-        result.setFirstName(user.getFistName());
-        result.setLastName(user.getLastName());
-        result.setStatus(user.getStatus());
-        result.setPhoneNumber(user.getPhoneNumber());
-        result.setTenantId(user.getTenantId());
-        result.setAccountType(user.getAccountType());
-        result.setHifaVerifying(user.getHifaVerifying() != null ? user.getHifaVerifying() : 0);
-
-        return result;
+        return new SecurityUser(user);
     }
 
 }
